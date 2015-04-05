510df8819a1cc20b87552dbd533bd69209880cc6
Koichiro Oishi <bs@v7.com>
2015-04-05 12:07:16 +0900

    Initial Commit

---
+++ SampleList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleList
+//
+//  Created by bs on 2015/04/05.
+//  Copyright (c) 2015年 bs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleList
+//
+//  Created by bs on 2015/04/05.
+//  Copyright (c) 2015年 bs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
