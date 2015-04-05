b2ad0267c089f24281cffcf5da7b200f76f35609
Koichiro Oishi <bs@v7.com>
2015-04-05 12:18:24 +0900

    add ListManager

---
+++ SampleList/ListManagerClass.h
@@ -0,0 +1,13 @@
+//
+//  ListManagerClass.h
+//  SampleList
+//
+//  Created by bs on 2015/04/05.
+//  Copyright (c) 2015年 bs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ListManagerClass : NSObject
+
+@end
---
+++ SampleList/ListManagerClass.m
@@ -0,0 +1,13 @@
+//
+//  ListManagerClass.m
+//  SampleList
+//
+//  Created by bs on 2015/04/05.
+//  Copyright (c) 2015年 bs. All rights reserved.
+//
+
+#import "ListManagerClass.h"
+
+@implementation ListManagerClass
+
+@end
